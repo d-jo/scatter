e4c35d3892629bd6b46ddebf3d8589b624ec4e94
Kyle Surowiec <kylesurowic@me.com>
2019-09-11 22:37:19 -0500

    Code clean up

--- decoding.js
+++ decoding.js
@@ -1,125 +1,125 @@
-
 const io = require("./io.js")
 
-var transforms = {
+const transforms = {
 	"index": indexTransform,
 	"keys": keyTransform,
 	"regex": regexTransform,
-}
-module.exports.Transformations = transforms
+};
 
-var decoding = {
+const decoding = {
 	// rsa
 	// sha3
 	// ...
-	raw: raw_decode,
-}
-module.exports.Decodings = decoding
+	raw: rawDecode,
+};
 
-async function regexTransform(data, manifest_entry) {
+function regexTransform(data, manifest_entry) {
 	return new Promise((resolve, reject) => {
 		try {
-			let ret
+			let ret;
 			data.match(manifest_entry["regex"]).forEach(v => {
-				ret += v
+				ret += v;
 			})
-			resolve(ret)
+			resolve(ret);
 		} catch (err) {
-			reject(err)
+			reject(err);
 		}
-	})
+	});
 }
-module.exports.RegexTransform = regexTransform
 
-async function indexTransform(data, manifest_entry) {
+function indexTransform(data, manifest_entry) {
 	return new Promise((resolve, reject) => {
 		try {
-			resolve(data.substring(manifest_entry["start"], manifest_entry["end"]))
+			resolve(data.substring(manifest_entry["start"], manifest_entry["end"]));
 		} catch (err) {
-			reject(err)
+			reject(err);
 		}
 	})
 }
-module.exports.IndexTransform = indexTransform
 
-async function keyTransform(data, manifest_entry) {
+function keyTransform(data, manifest_entry) {
 	return new Promise((resolve, reject) => {
 		try {
-			resolve(data.substring(data.indexOf(manifest_entry["start"]), data.indexOf(manifest_entry["end"])))
-		} catch(err) {
-			reject(err)
+			resolve(data.substring(data.indexOf(manifest_entry["start"]), data.indexOf(manifest_entry["end"])));
+		} catch (err) {
+			reject(err);
 		}
 	})
 }
-module.exports.KeyTansform = keyTransform
 
-async function raw_decode(data) {
+function rawDecode(data) {
 	return new Promise((resolve, reject) => {
-		let str = data
-		resolve(str)
-	})
+		let str = data;
+		resolve(str);
+	});
 }
-module.exports.RawDecode = raw_decode
 
-
-async function decode(mf) {
+function decode(mf) {
 	return new Promise((resolve, reject) => {
-		let sections = {}
-		let index = 0
-		let retrieved = 0
-		let header = false
+		let sections = {};
+		let index = 0;
+		let retrieved = 0;
+		let header = false;
 		// for each fragment
 		mf["fragments"].forEach((fr) => {
 			// get retrival method and call it
-			let retr = io.Procurements[fr["source_type"]]
-			console.log("Source      \t" + fr["source"])
-			console.log("Source type \t" + fr["source_type"])
-			console.log("Source size \t" + fr["size"])
-			console.log("==========")
+			let retr = io.procurements[fr["source_type"]];
+			console.log("Source      \t" + fr["source"]);
+			console.log("Source type \t" + fr["source_type"]);
+			console.log("Source size \t" + fr["size"]);
+			console.log("==========");
 
 			retr(fr).then(raw_data => {
 				// go through the raw data from the fragmnet
 				// and parse the different data sections
 				if (!header) {
-					console.log("Section\tSource\t\tSourceType\tActual\tExpected")
-					header = true
+					console.log("Section\tSource\t\tSourceType\tActual\tExpected");
+					header = true;
 				}
 				fr["datas"].forEach(datas_entry => {
 					// get the transformation
-					let transformation = transforms[datas_entry["type"]]
+					let transformation = transforms[datas_entry["type"]];
 					// apply the transformation
 					transformation(raw_data, datas_entry).then(section => {
-						sections[datas_entry["order"]] = section
-						retrieved += section.length
-						console.log(datas_entry["order"] + "\t" + fr["source"].substring(12, 24) + "\t" + fr["source_type"] + "\t\t" + section.length + "\t" + datas_entry["size"])
+						sections[datas_entry["order"]] = section;
+						retrieved += section.length;
+						console.log(datas_entry["order"] + "\t" + fr["source"].substring(12, 24) + "\t" + fr["source_type"] + "\t\t" + section.length + "\t" + datas_entry["size"]);
 						if (retrieved >= mf["size"]) {
-							resolve(sections)
+							resolve(sections);
 						}
 					}).catch(err2 => {
-						console.log("Error extracing data section from source " + fr["source"])
-						console.log("Error message: " + err2.message)
-						console.log(datas_entry["size"] + " Bits will be missing from data starting at " + retrieved)
-						let str
-						for(let i = 0; i < datas_entry["size"]; i++) {
-							str += "0"
+						console.log("Error extracing data section from source " + fr["source"]);
+						console.log("Error message: " + err2.message);
+						console.log(datas_entry["size"] + " Bits will be missing from data starting at " + retrieved);
+						let str;
+						for (let i = 0; i < datas_entry["size"]; i++) {
+							str += "0";
 						}
-						sections[datas_entry["order"]] = str
-						retrieved += datas_entry["size"]
-					})
-				})
+						sections[datas_entry["order"]] = str;
+						retrieved += datas_entry["size"];
+					});
+				});
 			}).catch(err => {
-				console.log("Error fetching source " + fr["source"])
-				console.log("Error message: " + err.message)	
-				console.log(fr["size"] + " bits will be missing from data starting at " + retrieved)
-				let str
-				for(let i = 0; i < datas_entry["size"]; i++) {
-					str += "0"
+				console.log("Error fetching source " + fr["source"]);
+				console.log("Error message: " + err.message);
+				console.log(fr["size"] + " bits will be missing from data starting at " + retrieved);
+				let str;
+				for (let i = 0; i < datas_entry["size"]; i++) {
+					str += "0";
 				}
-				sections[datas_entry["order"]] = str
-				retrieved += datas_entry["size"]
-			})
-		})
-	})
+				sections[datas_entry["order"]] = str;
+				retrieved += datas_entry["size"];
+			});
+		});
+	});
 }
-module.exports.Decode = decode
 
+module.exports = {
+	transforms,
+	decode,
+	decoding,
+	rawDecode,
+	keyTransform,
+	indexTransform,
+	regexTransform,
+};
