e2457ccc7fc1175ae55096e4b189b27737992e0c
d-jo <declan.l.johnson@gmail.com>
2019-09-18 11:49:08 -0500

    Decoding changes

--- decoding.js
+++ decoding.js
@@ -8,13 +8,13 @@ var transforms = {
 }
 module.exports.Transformations = transforms
 
-var decoding = {
+var decodings = {
 	// rsa
 	// sha3
 	// ...
 	raw: raw_decode,
 }
-module.exports.Decodings = decoding
+module.exports.Decodings = decodings
 
 async function regexTransform(data, manifest_entry) {
 	return new Promise((resolve, reject) => {
@@ -72,10 +72,6 @@ async function decode(mf) {
 		mf["fragments"].forEach((fr) => {
 			// get retrival method and call it
 			let retr = io.Procurements[fr["source_type"]]
-			console.log("Source      \t" + fr["source"])
-			console.log("Source type \t" + fr["source_type"])
-			console.log("Source size \t" + fr["size"])
-			console.log("==========")
 
 			retr(fr).then(raw_data => {
 				// go through the raw data from the fragmnet
@@ -89,12 +85,19 @@ async function decode(mf) {
 					let transformation = transforms[datas_entry["type"]]
 					// apply the transformation
 					transformation(raw_data, datas_entry).then(section => {
-						sections[datas_entry["order"]] = section
-						retrieved += section.length
-						console.log(datas_entry["order"] + "\t" + fr["source"].substring(12, 24) + "\t" + fr["source_type"] + "\t\t" + section.length + "\t" + datas_entry["size"])
-						if (retrieved >= mf["size"]) {
-							resolve(sections)
-						}
+						let decod = decodings[datas_entry["decoding"]]
+						decod(section).then(fd => {
+							sections[datas_entry["order"]] = fd
+							retrieved += fd.length // ADD DECODING HERE
+							console.log(datas_entry["order"] + "\t" + fr["source"].substring(12, 24) + "\t" + fr["source_type"] + "\t\t" + section.length + "\t" + datas_entry["size"])
+							if (retrieved >= mf["size"]) {
+								resolve(sections)
+							}
+						}).catch(err3 => {
+							console.log("Error decoding data section " + datas_entry["order"])
+							console.log("Using raw_decode as default")
+						
+						})
 					}).catch(err2 => {
 						console.log("Error extracing data section from source " + fr["source"])
 						console.log("Error message: " + err2.message)
