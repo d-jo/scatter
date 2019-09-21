35c677d0faf0a278e6ef8bed2dc6ada0c79a64a2
Declan <declan.l.johnson@gmail.com>
2019-09-21 18:57:26 -0400

    Trie!

--- encoding.js
+++ encoding.js
@@ -1,27 +1,73 @@
 
+function Node(data) {
+	this.data = data;
+	this.parent = null;
+	this.children = {};
+}
 
-function recursiveTreeBuild(trie, sections, index) {
-	let last = sections.length - 1;
-	if (index == last) {
-		// base case, return object
-		let key = sections[last];
-		// this doesnt work, it interprets key literally
-		return {key: {}};
-	} else {
-		// curr is actually next
-		let dest = trie[sections[index]];
-		// TODO fiinish this, not working
-		if (!dest) {
-			// path does not exist, create
-			trie[sections[index]] = {};
-			curr = trie[sections[index]];
-			return recursiveTreeBuild(curr[sections[index]], sections, index + 1);
+function Trie() {
+	let node = new Node("");
+	this._root = node;
+}
+
+function AddToTrie(trie, dataArr) {
+	// start with root node
+	let currNode = trie._root;
+	// for each element in input dataArr
+	outter:
+	for (let i = 0; i < dataArr.length; i++) {
+		// check to see if node has children given current element
+		if (!currNode.children[dataArr[i]]) {
+			// node does not have children of this input word
+			let newChild = new Node(dataArr[i]);
+			newChild.parent = currNode;
+			currNode.children[dataArr[i]] = [newChild];
+			currNode = newChild;
 		} else {
-			// path does exist, do not modify curr
-			return recursiveTreeBuild(curr[sections[index]], sections, index + 1);
+			// a list of children of this input word exists
+			// search it
+			let childNodes = currNode.children[dataArr[i]];
+			for (let j = 0; j < childNodes.length; j++) {
+				let n = childNodes[j];
+				if (n.data == dataArr[i]) {
+					// input word match
+					// advance without changes
+					currNode = n;
+					continue outter;
+				}
+			}
+			// getting here means the children 
+			// array does not contain a path for 
+			// this input word
+			let newChild = new Node(dataArr[i]);
+			newChild.parent = currNode;
+			currNode.children.push(newChild);
+			currNode = newChild;
 		}
 	}
 }
+
+
+function PrintTrie(node, d) {
+	//console.log(d + "|" + node.data)
+	process.stdout.write(d + " |");
+	for (let i = 0; i < d; i++) {
+		process.stdout.write("  ");
+	}
+	process.stdout.write(node.data + "\n");
+	Object.keys(node.children).forEach(c => {
+		node.children[c].forEach(nc => {
+			PrintTrie(nc, d+1);
+		})
+	})
+}
+
+// let t = new Trie();
+// ["horsing", "horse", "home", "hope", "hose", "hoses", "hosed"].forEach(e => {
+// 	let cs = e.split("");
+// 	AddToTrie(t, cs);
+// });
+// PrintTrie(t._root, 0)
 
 
 function createSearchObject(inputFile) {
